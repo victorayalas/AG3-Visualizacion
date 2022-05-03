9c3518614c43f3d7129d13f58e395638785b3632
victorayalas <v.ayala.s@hotmail.com>
2022-05-03 00:46:03 -0500

    1 file modified

--- index.js
+++ index.js
@@ -68,3 +68,15 @@ function visualiza(data){
   .order("Total")
   .draw()
   }
+
+d3.json("https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json", function(data) {
+var visualization = d3plus.viz()
+.container("#viz2")
+.data(data)
+.type('bar')
+.id('name')
+.x('year')
+.y('value')
+.axes({ ticks: 'false' })
+.draw();
+});
